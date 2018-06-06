b3c023d75eb2fd88d76b7cf4c4daf9c604ab4317
Alzheimancer <morleng2@gmail.com>
2018-06-06 14:32:00 +0700

    edit validator

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,52 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 // Load Input validation
-const validationAddInput = require('../../validation/add');
+const validationAddProduct = require("../../validation/add");
 
-const Product = require('../../models/Product');
+const Product = require("../../models/Product");
 
 // @route GET api/products/test
 // @desc Tests products route
 // @access Public
-router.get('/test', (req, res) => res.json({
-    msg: 'Products works'
-}));
+router.get("/test", (req, res) =>
+  res.json({
+    msg: "Products works"
+  })
+);
 
 // @route Post api/products/add
 // @desc Adds new product
 // @access Private
-router.post('/add', (req, res) => {
+router.post("/add", (req, res) => {
+  const { errors, isValid } = validationAddProduct(req.body);
 
-    const {
-        errors,
-        isValid
-    } = validationAddInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const newProduct = new Product({
+    number: req.body.number,
+    topic: req.body.topic,
+    type: req.body.type,
+    desc: req.body.desc,
+    category: req.body.category,
+    price: req.body.price,
+    location: {
+      latitude: req.body.location.latitude,
+      longitude: req.body.location.longitude,
+      province: req.body.location.province,
+      sector: req.body.location.sector
+    },
+    postedBy: req.body.postedBy
+  });
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    const newProduct = new Product({
-        number: req.body.number,
-        topic: req.body.topic,
-        type: req.body.type,
-        desc: req.body.desc,
-        category: req.body.category,
-        price: req.body.price,
-        location: {
-            latitude: req.body.location.latitude,
-            longitude: req.body.location.longitude,
-            province: req.body.location.province,
-            sector: req.body.location.sector,
-        },
-        postedBy: req.body.postedBy,
-    });
-
-    newProduct.save()
-        .then(product => res.json(product))
-        .catch(err => console.log(err));
-    // console.log(req.body.number);
+  newProduct
+    .save()
+    .then(product => res.json(product))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
--- validation/add.js
+++ validation/add.js
@@ -1,17 +1,14 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
 module.exports = function validationAddProduct(data) {
-    let errors = {};
-    if (Validator.isLength(data.topic, {
-            min: 2,
-            max: 30
-        })) {
-        errors.topic = "topic must be between 2 and 30 characters";
-    }
+  let errors = {};
+  if (Validator.isLength(data.topic, { min: 2, max: 30 })) {
+    errors.topic = "topic must be between 2 and 30 characters";
+  }
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    };
-}
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
